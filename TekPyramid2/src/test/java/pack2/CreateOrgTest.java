70b475a2de542a3e01126edf0747b3b7cd7940c1
mdsad <mdsad@LAPTOP-CUL491K4>
2025-03-11 17:20:38 +0530

    Parameters are added to the classes

--- TekPyramid2/src/test/java/pack2/CreateOrgTest.java
+++ TekPyramid2/src/test/java/pack2/CreateOrgTest.java
@@ -8,6 +8,16 @@ public class CreateOrgTest {
 
 		
 		System.out.println("execute createOrgTest");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 
 	}
 
